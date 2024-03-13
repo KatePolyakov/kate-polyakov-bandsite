c8120a9825f252ff40fdc85e4cff6ff1d8089f6d
Kate <polyakovkate@gmail.com>
2024-03-13 16:30:40 -0700

    added music Box

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,38 @@
+const dates = [
+  {
+    date: 'Mon Sept 09 2024',
+    place: 'Ronald Lane',
+    city: 'San Francisco, CA',
+    link: 'http//',
+  },
+  {
+    date: 'Tue Sept 17 2024',
+    place: 'Pier 3 East',
+    city: 'San Francisco, CA',
+    link: 'http//',
+  },
+  {
+    date: 'Sat Oct 12 2024',
+    place: 'View Lounge',
+    city: 'San Francisco, CA',
+    link: 'http//',
+  },
+  {
+    date: 'Sat Nov 16 2024',
+    place: 'Hyatt Agency',
+    city: 'San Francisco, CA',
+    link: 'http//',
+  },
+  {
+    date: 'Fri Nov 29 2024',
+    place: 'Moscow Center',
+    city: 'San Francisco, CA',
+    link: 'http//',
+  },
+  {
+    date: 'Wed Dec 18 2024',
+    place: 'Press Club',
+    city: 'San Francisco, CA',
+    link: 'http//',
+  },
+];
